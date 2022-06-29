390cb76ccd9b51d8d243ceb7a17f3713ea615c03
Thomas Brown <tommy.brown1125@gmail.com>
2022-06-29 02:20:46 -0400

    added styling to navbar, configuring firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyDuUQK4-UElWFqwDShMrWHStTvq1c1ePTc',
+    authDomain: 'crwn-db-d1251.firebaseapp.com',
+    projectId: 'crwn-db-d1251',
+    storageBucket: 'crwn-db-d1251.appspot.com',
+    messagingSenderId: '443585278029',
+    appId: '1:443585278029:web:873c71e5b280affd60ac04',
+    measurementId: 'G-K445DGYHFZ'
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Google Sign-in Configuration
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
