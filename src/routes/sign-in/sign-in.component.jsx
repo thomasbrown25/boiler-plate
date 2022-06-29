6d013a926bbe5088795065c9d9105474416117cc
Thomas Brown <tommy.brown1125@gmail.com>
2022-06-29 02:59:08 -0400

    added google redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 import {
     signInWithGooglePopup,
-    createUserDocumentFromAuth
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect,
+    auth
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    useEffect(() => {
+        (async () => {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(
+                    response.user
+                );
+            }
+        })();
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +28,9 @@ const SignIn = () => {
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button>
         </div>
     );
 };
