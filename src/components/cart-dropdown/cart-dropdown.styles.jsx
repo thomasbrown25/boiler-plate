709fa9e1c7a95d2e8d3592463276a980d7e8b31f
Thomas Brown <thobrown@microsoft.com>
2022-07-11 12:47:03 -0400

    finishing some touches

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+import {
+    BaseButton,
+    GoogleSignInButton,
+    InvertedButton
+} from '../button/button.styles';
+
 export const CartDropdownContainer = styled.div`
     position: absolute;
-    width: 240px;
+    width: 270px;
     height: 340px;
     display: flex;
     flex-direction: column;
@@ -12,6 +18,11 @@ export const CartDropdownContainer = styled.div`
     top: 90px;
     right: 40px;
     z-index: 5;
+    ${BaseButton},
+    ${GoogleSignInButton},
+  ${InvertedButton} {
+        margin-top: auto;
+    }
 `;
 
 export const EmptyMessage = styled.span`
@@ -25,34 +36,3 @@ export const CartItems = styled.div`
     flex-direction: column;
     overflow: scroll;
 `;
-
-// .cart-dropdown-container {
-//     position: absolute;
-//     width: 240px;
-//     height: 340px;
-//     display: flex;
-//     flex-direction: column;
-//     padding: 20px;
-//     border: 1px solid black;
-//     background-color: white;
-//     top: 90px;
-//     right: 40px;
-//     z-index: 5;
-
-//     .empty-message {
-//         font-size: 18px;
-//         margin: 50px auto;
-//     }
-
-//     .cart-items {
-//         height: 240px;
-//         display: flex;
-//         flex-direction: column;
-//         overflow: scroll;
-//         overflow-x: hidden;
-//     }
-
-//     button {
-//         margin-top: auto;
-//     }
-// }
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,9 +2,7 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles';
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
